9d656bc4f5bae7e8e0fe6e05a5e21d09b54f55f8
Kasun Miuranga <kasunmiuranga2000@gmail.com>
2024-01-13 09:44:44 +0530

    mongo db connect

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// connect to db
+const dbConnection = require('./db/DBConnection');
+dbConnection();
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
